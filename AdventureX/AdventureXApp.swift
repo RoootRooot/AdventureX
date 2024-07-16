b6c43eee0b4156d5daf0e4ba9bb917a37e227b1e
GH <BoneAsh@iCloud.com>
2024-07-16 15:00:12 +0800

    Initial Commit

---
+++ AdventureX/AdventureXApp.swift
@@ -0,0 +1,34 @@
+//
+//  AdventureXApp.swift
+//  AdventureX
+//
+//  Created by GH on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AdventureXApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ AdventureX/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  AdventureX
+//
+//  Created by GH on 7/16/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
